69c955261aceb2e6334802dcb785063b3ed2f2ab
Drackorr <axel.rotthier@imie.fr>
2017-11-09 10:09:49 +0100

    suite

---
+++ 20000_lieu.py
@@ -0,0 +1,33 @@
+#-- coding: utf-8 --
+with open('20000.txt','r',encoding='utf8') as fichier:
+    f = fichier.read()
+    #print(f)
+
+
+def nettoyer_texte(texte):
+    texte = texte.lower()
+    car_spec = ",.!?()\";:"
+    for lettre in texte:
+        if lettre in car_spec:
+            texte = texte.replace(lettre,'')
+    texte = texte.replace('\n',' ')
+    texte = texte.replace("'", ' ')
+    return texte
+
+def texte_en_liste(texte):
+    liste_de_mots = texte.split(' ')
+    liste_de_mots = list(filter(None,liste_de_mots))
+    return liste_de_mots
+
+def compter_mots_unique(liste):
+    liste_de_mots_uniques = list(set(liste))
+    return len(liste_de_mots_uniques)
+
+
+texte_nettoyer = nettoyer_texte(f)
+liste_de_mots = texte_en_liste(texte_nettoyer)
+nb_mots = len(liste_de_mots)
+nb_mots_uniques = compter_mots_unique(liste_de_mots)
+
+print(nb_mots)
+print(nb_mots_uniques)
--- 20000_lieux.py
+++
@@ -1,4 +0,0 @@
-#-*- coding: utf-8 -*-
-with open('20000.txt','r',encoding='utf-8') as fichier:
-    f1 = fichier.readline()
-    f2 = fichier.readline()
